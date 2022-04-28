d850b67926d5d881248bbb80a789aee57af23647
Gabriel Hanich <68582067+gabriel-hanich@users.noreply.github.com>
2022-04-28 18:27:10 +1000

    Added sidebar

--- ng-project/src/app/app.module.ts
+++ ng-project/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home/home-page/home-page.component';
 import { WelcomeComponent } from './components/home/welcome/welcome.component';
+import { SidebarComponentComponent } from './components/sidebar/sidebar-component/sidebar-component.component';
+import { SidebarToggleComponent } from './components/sidebar/sidebar-toggle/sidebar-toggle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    SidebarComponentComponent,
+    SidebarToggleComponent
   ],
   imports: [
     BrowserModule,
--- ng-project/src/app/components/home/welcome/welcome.component.ts
+++ ng-project/src/app/components/home/welcome/welcome.component.ts
@@ -8,10 +8,43 @@ import { GlobalVarsService } from 'src/app/services/global-vars.service';
 })
 export class WelcomeComponent implements OnInit {
   public userName: string = this.globalVars.getVar("userName");
+  public greetingText: string;
+  public timeText: string = "00:00";
 
   constructor(private globalVars: GlobalVarsService) { }
 
   ngOnInit(): void {
+    this.updateTime();
+    const loginTime = new Date();
+    if(loginTime.getHours() < 10){
+      this.greetingText = "Good Morning"
+    }else if(loginTime.getHours() < 15){
+      this.greetingText = "Hello"
+    }else if(loginTime.getHours() < 18){
+      this.greetingText = "Good Afternoon"
+    }else{
+      this.greetingText = "Good Evening"
+    }
+
+    setInterval(()=>{
+      this.updateTime();
+    }, 1000)
+  }
+
+  updateTime(): void{
+    var currentTime: Date = new Date();
+      var suffix: string = "am";
+      var hours: number = currentTime.getHours()
+      if(hours > 12){
+        hours -= 12;
+        suffix = "pm"
+      }
+      var minutes: string = currentTime.getMinutes().toString();
+      if(minutes.length == 1){
+        minutes = "0" + minutes
+      }
+
+      this.timeText = hours.toString() + ":" + minutes + suffix;
   }
 
 }
