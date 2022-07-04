8cfd7171372e308c94e6ac12f431d964647f476d
Gabriel Hanich <68582067+gabriel-hanich@users.noreply.github.com>
2022-07-04 15:38:44 +1000

    Finalised application for beta release

--- ng-project/src/environments/environment.prod.ts
+++ ng-project/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   version: "0.13",
-  build: "dev",
+  build: "beta",
   device: "angular",
   apiURL: "https://ap-southeast-2.aws.data.mongodb-api.com/app/base-camp-ttgwf/endpoint",
   keysToSync: ["userName", "email", "lastSignInTime", "wk1Data", "wk2Data", "wk1IsWkA", "noteList", "studyNoteList", "widgetsLayout"]
